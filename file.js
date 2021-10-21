be9f5af81b05b9c9ef65ee910c292c45cdae9ce4
omar962001 <81726382+omar962001@users.noreply.github.com>
2021-10-21 02:46:05 +0200

    Add files via upload

---
+++ file.js
@@ -0,0 +1,9 @@
+var mypagecanvas = document.createElement('canvas');
+
+mypagecanvas.id = 'c';
+
+mypagecanvas.width = 600;
+mypagecanvas.height = 100;
+mypagecanvas.style.display = 'block';
+mypagecanvas.style.margin = '50px auto';
+document.body.appendChild(mypagecanvas);
